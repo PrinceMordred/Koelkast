8ac1fd8d59b6cc49e2be4baf7739f733f56e210d
PrinceMordred <jackrosenberg03@gmail.com>
2022-06-25 20:20:37 +0200

    finally done

--- Koelkast/Program.cs
+++ Koelkast/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Drawing;
+using System.Collections.Generic;
 
 namespace Koelkast
 {
@@ -19,8 +19,11 @@ namespace Koelkast
             byte[,] map = new byte[b, h];
 
             Queue<uint> qSet = new Queue<uint>();
-            Point koelkastPos = new Point();
-            Point koen = new Point();
+            int koelkastPosX = 0;
+            int koelkastPosY = 0;
+            int koenX = 0;
+            int koenY = 0;
+
             //find positions
             for (int i = 0; i < h; i++)
             {
@@ -33,14 +36,16 @@ namespace Koelkast
                             map[j, i] = 1;
                             break;
                         case '!':
-                            koelkastPos = new Point(j, i);
+                            koelkastPosX = j ;
+                            koelkastPosY = i;
                             map[j, i] = 2;
                             break;
                         case '?':
                             map[j, i] = 3;
                             break;
                         case '+':
-                            koen = new Point(j, i);
+                            koenX = j;
+                            koenY = i;
                             map[j, i] = 4;
                             break;
                         default:
@@ -49,10 +54,10 @@ namespace Koelkast
                     }
                 }
             }
-
+            
             //print solution
 
-            uint state = BreadthFirstSearch(map, koen, koelkastPos, qSet);
+            uint state = BreadthFirstSearch(map, koenX, koenY, koelkastPosX, koelkastPosY, qSet);
             if (state == 0)
                 Console.WriteLine("No solution");
             else
@@ -64,11 +69,11 @@ namespace Koelkast
             }
         }
 
-        private static uint BreadthFirstSearch(byte[,] arr, Point koen, Point koelkast, Queue<uint> queue)
+        private static uint BreadthFirstSearch(byte[,] arr, int koenX, int koenY, int koelkastX, int koelKastY, Queue<uint> queue)
         {
 
             //check if node is searched
-            uint state = State(koen, koelkast);
+            uint state = State(koenX, koenY, koelkastX, koelKastY);
             hSet.Add(state, 0);
             queue.Enqueue(state);
             while (queue.Count > 0)
@@ -79,19 +84,18 @@ namespace Koelkast
                 {
                     if (v == 0)
                         continue;
-                    Point kkp = KoelkastPos(v);
-                    if (arr[kkp.X, kkp.Y] == 3)
+                    (int kkpX, int kkpY) = KoelkastPos(v);
+                    if (arr[kkpX, kkpY] == 3)
                     {
                         hSet.Add(v, u);
                         return v;
                     }
 
-                    try
+                    if (!hSet.ContainsKey(v))
                     {
                         hSet.Add(v, u);
                         queue.Enqueue(v);
                     }
-                    catch { };
                 }
             }
             return 0;
@@ -109,22 +113,23 @@ namespace Koelkast
 
         static uint Step(uint state, int right, int down, byte[,] arr)
         {
-            Point kkp = KoelkastPos(state);
-            Point kp = KoenPos(state);
 
-            kp.X += right;
-            kp.Y += down;
+            (int kkpX,int  kkpY) = KoelkastPos(state);
+            (int kpX, int kpY) = KoenPos(state);
 
-            if (arr[kp.X, kp.Y] == 0)
+            kpX += right;
+            kpY += down;
+
+            if (arr[kpX, kpY] == 0)
                 return 0;
-            if (kkp.X == kp.X && kkp.Y == kp.Y)
+            if (kkpX == kpX && kkpY == kpY)
             {
-                kkp.X += right;
-                kkp.Y += down;
-                if (arr[kkp.X, kkp.Y] == 0)
+                kkpX += right;
+                kkpY += down;
+                if (arr[kkpX, kkpY] == 0)
                     return 0;  
             }
-            return State(kp, kkp);
+            return State(kpX, kpY, kkpX, kkpY);
                 
         }
         static string printMap(char[,] m)
@@ -140,13 +145,13 @@ namespace Koelkast
             }
             return outp;
         }
-        static uint State(Point koen, Point koelkast)
+        static uint State(int koenX, int koenY, int koelkastX, int koelkastY)
         {
             // (koen)(koelkast)
-            return (uint)(koen.X | (koen.Y << 8) | (koelkast.X << 16) | (koelkast.Y << 24)) ;
+            return (uint)(koenX | (koenY << 8) | (koelkastX << 16) | (koelkastY << 24)) ;
         }
-        static Point KoelkastPos(uint state) => new Point((int)(state >> 16) & 255, (int)(state >> 24) & 255);
-        static Point KoenPos(uint state) => new Point((int)state & 255, (int)(state >> 8) & 255);
+        static (int, int) KoelkastPos(uint state) => ((int)(state >> 16) & 255, (int)(state >> 24) & 255);
+        static (int, int) KoenPos(uint state) => ((int)state & 255, (int)(state >> 8) & 255);
         static string Directions(uint endState)
         {
             string res = "";
@@ -154,13 +159,14 @@ namespace Koelkast
             uint current = hSet[endState];
             while (current != 0)
             {
-                Point kC = KoenPos(current);
-                Point kO = KoenPos(old);
-                Point offset = new Point(kO.X - kC.X,  kO.Y- kC.Y);
+                (int kCX, int kCY) = KoenPos(current);
+                (int kOX, int kOY)= KoenPos(old);
+                int offsetX = (kOX - kCX);
+                int offsetY = (kOY- kCY);
                 old = current;
                 current = hSet[current];
 
-                res = "NWXES"[offset.X + offset.Y * 2 + 2] + res; //credits Olaf, we hadden samen bedacht dat strings arrays zijn <3 (dit is geen plagiaat)
+                res = "NWXES"[offsetX + offsetY * 2 + 2] + res; //credits Olaf, we hadden samen bedacht dat strings arrays zijn <3 (dit is geen plagiaat)
             }
             return res;
         }
